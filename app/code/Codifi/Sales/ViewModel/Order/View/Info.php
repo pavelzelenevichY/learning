26b38a0dd5f3fded8f426c514390ad33f937e63a
pavelzelenevichY <pzelenevich@codifi.me>
2021-04-23 18:47:17 +0300

    mmt-32 Correction. Added latest changes. Code style.

--- app/code/Codifi/Sales/ViewModel/Order/View/Info.php
+++ app/code/Codifi/Sales/ViewModel/Order/View/Info.php
@@ -6,29 +6,27 @@
  * @author      Pavel Zelenevich <pzelenevich@codifi.me>
  */
 
-namespace Codifi\Sales\Block\Adminhtml\Order\View;
+namespace Codifi\Sales\ViewModel\Order\View;
 
+use Codifi\Sales\Model\Source\OrderType;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Sales\Block\Adminhtml\Order\AbstractOrder;
 use Magento\Backend\Block\Template\Context;
 use Magento\Framework\Registry;
 use Magento\Sales\Helper\Admin;
-use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Class Info
- * @package Codifi\Sales\Block\Adminhtml\Order\View
+ * @package Codifi\Sales\ViewModel\Order\View
  */
-class Info extends AbstractOrder
+class Info extends AbstractOrder implements ArgumentInterface
 {
     /**
-     * Options
+     * Order type options
      *
-     * @var array[]
+     * @var OrderType
      */
-    private $options = [
-        ['value' => 'REGULAR', 'label' => 'Regular Order', 'selected' => 1],
-        ['value' => 'CREDIT_HOLD', 'label' => 'Credit Hold Order', 'selected' => 0]
-    ];
+    private $orderType;
 
     /**
      * Info constructor.
@@ -36,34 +34,32 @@ class Info extends AbstractOrder
      * @param Context $context
      * @param Registry $registry
      * @param Admin $adminHelper
+     * @param OrderType $orderType
      * @param array $data
      */
     public function __construct(
         Context $context,
         Registry $registry,
         Admin $adminHelper,
+        OrderType $orderType,
         array $data = []
     ){
-        parent::__construct(
-            $context,
-            $registry,
-            $adminHelper,
-            $data
-        );
+        $this->orderType = $orderType;
+        parent::__construct($context, $registry, $adminHelper, $data);
     }
+
 
     /**
      * Get order_type attribute value
      *
-     * @return array
-     * @throws LocalizedException
+     * @return array[]
      */
     public function getAttributeValue()
     {
         $response = [];
         $order = $this->getOrder();
         $orderType = $order->getData('order_type');
-        $options = $this->options;
+        $options = $this->orderType->getAllOptions();
         foreach ($options as $item){
             if ($item['value'] === $orderType) {
                 $response[] = $item;
