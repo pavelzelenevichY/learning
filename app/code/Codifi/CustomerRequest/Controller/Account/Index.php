4164ce0844b0776411b3ddbfe3427f056aa4b918
pavelzelenevichY <pzelenevich@codifi.me>
2021-05-05 09:19:19 +0300

    mmt-37 Added new tab in account navigation.

---
+++ app/code/Codifi/CustomerRequest/Controller/Account/Index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Codifi_CustomerRequest
+ *
+ * @copyright   Copyright (c) 2021 Codifi
+ * @author      Pavel Zelenevich <pzelenevich@codifi.me>
+ */
+
+namespace Codifi\CustomerRequest\Controller\Account;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\View\Result\Page;
+
+/**
+ * Class Index
+ * @package Codifi\CustomerRequest\Controller\Account
+ */
+class Index extends Action
+{
+    /**
+     * Result page factory
+     *
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute function
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('Send request'));
+
+        return $resultPage;
+    }
+}
