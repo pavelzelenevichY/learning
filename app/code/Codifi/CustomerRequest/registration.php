2c7985d57f00d759e569c2e773849a96336c4490
Iryna Pshennik <iryna.pshennik@codifi.me>
2021-05-13 22:20:29 +0300

    Revert 'Merge pull request #12 from pavelzelenevichY/mmt-51-add-additional-condition'

---
+++ app/code/Codifi/CustomerRequest/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Codifi_CustomerRequest
+ *
+ * @copyright   Copyright (c) 2021 Codifi
+ * @author      Pavel Zelenevich <pzelenevich@codifi.me>
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Codifi_CustomerRequest',
+    __DIR__
+);
