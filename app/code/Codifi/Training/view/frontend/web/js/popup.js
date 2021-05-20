22ed262528d2faacba4533783287bed7b8a29210
pavelzelenevichY <pzelenevich@codifi.me>
2021-05-20 15:20:38 +0300

    mmt-57 Correction js.

--- app/code/Codifi/Training/view/frontend/web/js/popup.js
+++ app/code/Codifi/Training/view/frontend/web/js/popup.js
@@ -1,49 +1,53 @@
 /**
- * Codifi_CustomerRequest
+ * Codifi_Training
  *
  * @copyright   Copyright (c) 2021 Codifi
  * @author      Pavel Zelenevich <pzelenevich@codifi.me>
  */
 
-require([
+define([
     'jquery',
+    'uiComponent',
     'Magento_Customer/js/customer-data',
     'Magento_Ui/js/modal/confirm',
-], function ($, customerData) {
+], function ($, Component, customerData) {
     'use strict';
 
-    customerData.reload('credit_hold_section');
+    return Component.extend({
+        initialize: function () {
+            this._super();
+            customerData.reload('credit_hold_section');
+            this.customsection = customerData.get('credit_hold_section');
 
-    var customsection = customerData.get('credit_hold_section');
-    customsection.subscribe(function (updatedCustomer)
-    {
-        console.log(updatedCustomer);
-        var creditHold = updatedCustomer.credit_hold;
-
-        if (creditHold) {
-            $('.confirmation-modal-content').confirm({
-                actions: {
-                    always: function () {
-                        $.ajax({
-                            type: "POST",
-                            url: this.url,
-                            data: {
-                                'note': $.mage.__('Customer is notified about credit hold.'),
-                                'customer_id': this.customerId
-                            },
-                            success: function (data) {
+            this.customsection.subscribe(function (updatedCustomer) {
+                var creditHold = updatedCustomer.credit_hold;
+                if (creditHold) {
+                    $('.confirmation-modal-content').show();
+                    $('.confirmation-modal-content').confirm({
+                        actions: {
+                            always: function () {
+                                $.ajax({
+                                    type: "POST",
+                                    url: this.url,
+                                    data: {
+                                        'note': $.mage.__('Customer is notified about credit hold.'),
+                                        'customer_id': this.customerId
+                                    },
+                                    success: function (data) {
+                                    }
+                                });
                             }
-                        });
-                    }
-                },
-                buttons: [{
-                    text: $.mage.__('OK'),
-                    class: 'action primary action-accept',
-                    click: function (event) {
-                        this.closeModal(event, true);
-                    }
-                }]
+                        },
+                        buttons: [{
+                            text: $.mage.__('OK'),
+                            class: 'action primary action-accept',
+                            click: function (event) {
+                                this.closeModal(event, true);
+                            }
+                        }]
+                    });
+                }
             });
         }
-    }, this);
+    });
 });
