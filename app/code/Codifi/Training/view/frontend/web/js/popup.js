196a0e1f810333b92a6d54e88a951dd17bdbd4b5
pavelzelenevichY <pzelenevich@codifi.me>
2021-05-26 14:26:42 +0300

    mmt-57 Correction js.

--- app/code/Codifi/Training/view/frontend/web/js/popup.js
+++ app/code/Codifi/Training/view/frontend/web/js/popup.js
@@ -18,36 +18,78 @@ define([
             this._super();
             customerData.reload('credit_hold_section');
             this.customsection = customerData.get('credit_hold_section');
+            this.creditHold = this.customsection().credit_hold;
+
+            var url = this.url;
+            var customerId = this.customerId;
+
+            if (this.creditHold) {
+                showPopup();
+            }
 
             this.customsection.subscribe(function (updatedCustomer) {
-                var creditHold = updatedCustomer.credit_hold;
-                if (creditHold) {
-                    $('.confirmation-modal-content').show();
-                    $('.confirmation-modal-content').confirm({
-                        actions: {
-                            always: function () {
-                                $.ajax({
-                                    type: "POST",
-                                    url: this.url,
-                                    data: {
-                                        'note': $.mage.__('Customer is notified about credit hold.'),
-                                        'customer_id': this.customerId
-                                    },
-                                    success: function (data) {
-                                    }
-                                });
-                            }
-                        },
-                        buttons: [{
-                            text: $.mage.__('OK'),
-                            class: 'action primary action-accept',
-                            click: function (event) {
-                                this.closeModal(event, true);
-                            }
-                        }]
-                    });
+                this.creditHold = updatedCustomer.credit_hold;
+                if (this.creditHold) {
+                    showPopup();
                 }
             });
+
+            function showPopup(){
+                $('.confirmation-modal-content').show();
+                $('.confirmation-modal-content').confirm({
+                    actions: {
+                        always: function () {
+                            $.ajax({
+                                type: "POST",
+                                url: url,
+                                data: {
+                                    'note': $.mage.__('Customer is notified about credit hold.'),
+                                    'customer_id': customerId
+                                },
+                                success: function (data) {
+                                }
+                            });
+                        }
+                    },
+                    buttons: [{
+                        text: $.mage.__('OK'),
+                        class: 'action primary action-accept',
+                        click: function (event) {
+                            this.closeModal(event, true);
+                        }
+                    }]
+                });
+            }
+
+            // this.customsection.subscribe(function (updatedCustomer) {
+            //     var creditHold = updatedCustomer.credit_hold;
+            //     if (creditHold) {
+            //         $('.confirmation-modal-content').show();
+            //         $('.confirmation-modal-content').confirm({
+            //             actions: {
+            //                 always: function () {
+            //                     $.ajax({
+            //                         type: "POST",
+            //                         url: this.url,
+            //                         data: {
+            //                             'note': $.mage.__('Customer is notified about credit hold.'),
+            //                             'customer_id': this.customerId
+            //                         },
+            //                         success: function (data) {
+            //                         }
+            //                     });
+            //                 }
+            //             },
+            //             buttons: [{
+            //                 text: $.mage.__('OK'),
+            //                 class: 'action primary action-accept',
+            //                 click: function (event) {
+            //                     this.closeModal(event, true);
+            //                 }
+            //             }]
+            //         });
+            //     }
+            // });
         }
     });
 });
