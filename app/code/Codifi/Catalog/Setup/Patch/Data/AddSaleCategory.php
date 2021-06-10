bffc645f3ea26d57ad728b9d92b9ae3ca3e06ce5
pavelzelenevichY <pzelenevich@codifi.me>
2021-06-10 13:51:15 +0300

    mmt-49 Correction. Code style.

--- app/code/Codifi/Catalog/Setup/Patch/Data/AddSaleCategory.php
+++ app/code/Codifi/Catalog/Setup/Patch/Data/AddSaleCategory.php
@@ -11,18 +11,11 @@ declare(strict_types=1);
 namespace Codifi\Catalog\Setup\Patch\Data;
 
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Catalog\Model\Category;
 use Magento\Catalog\Model\CategoryFactory;
 use Magento\Catalog\Model\CategoryRepository;
-use Magento\Framework\App\State;
-use Magento\Store\Model\Store;
-use Magento\Store\Model\StoreManager;
 use Magento\UrlRewrite\Model\ResourceModel\UrlRewriteCollectionFactory;
 use Magento\Framework\Exception\CouldNotSaveException;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Api\FilterBuilder;
-use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+
 
 /**
  * Class AddSaleCategory
@@ -31,11 +24,9 @@ use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 class AddSaleCategory implements DataPatchInterface
 {
     /**
-     * Store manager
-     *
-     * @var StoreManager
+     * Column name category id
      */
-    private $storeManager;
+    const COLUMN_NAME_CATEGORY_ID = 'entity_id';
 
     /**
      * Category factory
@@ -52,13 +43,6 @@ class AddSaleCategory implements DataPatchInterface
     private $categoryRepository;
 
     /**
-     * State
-     *
-     * @var State
-     */
-    private $state;
-
-    /**
      * Url rewrite collection factory
      *
      * @var UrlRewriteCollectionFactory
@@ -68,23 +52,17 @@ class AddSaleCategory implements DataPatchInterface
     /**
      * AddSaleCategory constructor.
      *
-     * @param StoreManager $storeManager
      * @param CategoryFactory $categoryFactory
      * @param CategoryRepository $categoryRepository
-     * @param State $state
      * @param UrlRewriteCollectionFactory $urlRewriteCollectionFactory
      */
     public function __construct(
-        StoreManager $storeManager,
         CategoryFactory $categoryFactory,
         CategoryRepository $categoryRepository,
-        State $state,
         UrlRewriteCollectionFactory $urlRewriteCollectionFactory
     ) {
-        $this->storeManager = $storeManager;
         $this->categoryFactory = $categoryFactory;
         $this->categoryRepository = $categoryRepository;
-        $this->state = $state;
         $this->urlRewriteCollectionFactory = $urlRewriteCollectionFactory;
     }
 
@@ -92,31 +70,18 @@ class AddSaleCategory implements DataPatchInterface
      * Add new Sale category
      *
      * @throws CouldNotSaveException
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
      */
     public function apply(): void
     {
-        $urlKey = 'url-key-for-sale-category';
-        $row = [
-            'name'            => 'Sale',
-            'url_key'         => $urlKey,
-            'active'          => 1,
-            'is_anchor'       => 1,
-            'include_in_menu' => 1,
-            'display_mode'    => 'PAGE',
-        ];
-
-        $this->state->setAreaCode('adminhtml');
         $parentId = $this->getParentCategoryId();
 
         $data = [
             'parent_id'       => $parentId,
-            'name'            => $row['name'],
-            'is_active'       => $row['active'],
-            'is_anchor'       => $row['is_anchor'],
-            'include_in_menu' => $row['include_in_menu'],
-            'url_key'         => $row['url_key'],
+            'name'            => 'Sale',
+            'is_active'       => 1,
+            'is_anchor'       => 1,
+            'include_in_menu' => 1,
+            'url_key'         => 'sale',
         ];
 
         $category = $this->categoryFactory->create();
@@ -126,18 +91,18 @@ class AddSaleCategory implements DataPatchInterface
         $this->categoryRepository->save($category);
     }
 
-
     /**
      * Get parent category id
      *
      * @return int
      */
-    private function getParentCategoryId(): int
+    public function getParentCategoryId(): int
     {
         $urlCollection = $this->urlRewriteCollectionFactory->create();
         $urlCollection->addFilter('request_path', 'gear/fitness-equipment.html');
-
-        $id = $urlCollection->getData()[0]['entity_id'] ?? 0;
+        $urlCollection->setPageSize(1);
+        $firstItem = $urlCollection->getFirstItem();
+        $id = $firstItem->getDataByKey(self::COLUMN_NAME_CATEGORY_ID);
 
         return (int)$id;
     }
